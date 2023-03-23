113a50eff1d5a512491315722bd804c671897687
Alaa Barka <tmakaveli643@gmail.com>
2023-03-23 15:42:40 +0000

    starting the typescript project

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from "express"
+import "dotenv/config"
+import { json } from "body-parser"
+import appDataSource from "./utils/POSTGRES"
+const cors = require("cors")
+const app = express()
+
+
+
+app.use(json())
+app.use(cors())
+
+const PORT: any = process.env.PORT || 3000
+
+app
+  .listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+
+    appDataSource
+      .initialize()
+      .then((res: any) => {
+        console.log("Connected to the database ! ")
+        //app use
+      })
+      .catch((e: any) => {
+        console.log(e)
+      })
+  })
+  .on("error", () => {
+    console.log("There's an error ! ")
+  })
